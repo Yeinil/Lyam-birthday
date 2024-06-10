eb1cfcc105668a034cf19df00c59f3c9117f7136
Yeinil <yeinilfabian@gmail.com>
2024-06-10 10:23:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    // Any JavaScript functionality can be added here if needed
+$(document).ready(function() {
+    $('#rsvp-form').on('submit', function(e) {
+        e.preventDefault();
+        var name = $('#name').val();
+        if (name) {
+            alert('¡Gracias por confirmar tu asistencia, ' + name + '!');
+            $('#rsvp-form')[0].reset();
+        } else {
+            alert('Por favor, introduce tu nombre.');
+        }
+    });
 });
